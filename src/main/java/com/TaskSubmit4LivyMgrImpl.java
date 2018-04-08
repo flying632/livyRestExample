29bd6bbab0e494d1067f1e241eb581f0ce6342e7
baosong <13793883820@163.com>
2018-04-08 19:27:11 +0800

    three

--- src/main/java/com/TaskSubmit4LivyMgrImpl.java
+++ src/main/java/com/TaskSubmit4LivyMgrImpl.java
@@ -7,19 +7,18 @@ import java.util.logging.Logger;
 /**
  * 向yarn 上提交Spark 任务
  */
-public class TaskSubmit4LivyMgrImpl implements BatchMgr {
+public class TaskSubmit4LivyMgrImpl{
 
 //   private static Logger log = Logger.getLogger(TaskSubmit4LivyMgrImpl.class);
-    @Override
     //通过Livy 提交spark 任务到yarn 集群
-    public void invoke(JobExecutionContext context) {
-//        log.info("~~ invoke task");
-        String url = "http://172.17.11.11:8998/batches";
-        String postData = "{\"kind\": \"spark\"}";
-        String pData = "{\"file\":\"D://Pi.jar\",\"className\":\"neu.edu.cn.PiJob\" }";
-        String reqResult = ReqEngine.sendPostReq(url, pData);
-        System.out.println(reqResult);
-    }
+//    public void invoke(JobExecutionContext context) {
+////        log.info("~~ invoke task");
+//        String url = "http://172.17.11.11:8998/batches";
+//        String postData = "{\"kind\": \"spark\"}";
+//        String pData = "{\"file\":\"D://Pi.jar\",\"className\":\"neu.edu.cn.PiJob\" }";
+//        String reqResult = ReqEngine.sendPostReq(url, pData);
+//        System.out.println(reqResult);
+//    }
     public static void main(String[] args){
 
         String url = "http://172.17.11.170:8998/batches";
